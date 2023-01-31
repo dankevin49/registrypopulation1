0b9cf21a3ed9563f450ac56272365067795506af
dankevin49 <dankevi.camacho49@gmail.com>
2023-01-31 12:12:27 +0800

    first commit

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+<main>
+    <div class="flex flex-col sm:justify-top items-center pt-6 sm:pt-120">
+        <div id="logo" class="w-80">
+        <img src="images/bor.png">
+    </div>
+
+        <div
+            class="w-full h-full sm:max-w-md mt-4 px-4 py-4 bg-gray-100 shadow-md overflow-hidden sm:rounded-lg"
+        >
+            <slot />
+        </div>
+    </div>
+</main>
+</template>
+
+<style lang="css">
+main{
+    background-image: linear-gradient(#37b524d4 0.14%, #e91155da 46.77%, #37b524d4 92.2%);
+    background-position: center;
+    background-size: cover;
+    position: relative;
+    width: 100%;
+    min-height: 100vh;
+    }
+</style>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+<template>
+<main>
+    <div class="relative flex items-top justify-center min-h-screen">
+        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <Link
+                    v-if="$page.props.auth.user"
+                    :href="route('dashboard')"
+                    class="text-sm text-white-700 text-white-500 underline"
+                    >Dashboard</Link
+                >
+
+                <template v-else>
+                    <Link :href="route('login')" class="text-sm text-gray-0">Log in</Link>
+
+                    <Link
+                        v-if="canRegister"
+                        :href="route('register')"
+                        class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"
+                        >Register</Link
+                    >
+                </template>
+        </div>
+    </div>
+</main>
+</template>
+
+<style lang="css">
+main{
+    background-image: linear-gradient(#37b524d4 0.14%, #e91155da 46.77%, #37b524d4 92.2%);
+    background-position: center;
+    background-size: cover;
+    position: relative;
+    width: 100%;
+    min-height: 100vh;
+    }
+</style>
